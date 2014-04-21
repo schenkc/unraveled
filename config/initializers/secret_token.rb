d28e02c91ae7d55029e81028167f17a00193b179
candace <candace.schenk@gmail.com>
2014-04-21 14:12:56 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unraveled::Application.config.secret_key_base = '84ee7aa58c8266586fdb966a4e5361d76a8228356bc614edb9889029796088bc87bdf0acd1604fcba13eef9755bc8ad3593d4749c4ef861d19886cc8616356bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unraveled::Application.config.session_store :cookie_store, key: '_unraveled_session'
