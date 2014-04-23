c1a85510002b881e5afddb8fb955da9bbdb3ab6e
Candace Schenk <candace.schenk@gmail.com>
2014-04-22 21:18:34 -0400

    finishes outline for pattern model (still need to do file upload)

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -18,24 +18,38 @@ class PatternsController < ApplicationController
   end
 
   def index
+    @patterns = Pattern.all
+    render :index
   end
 
   def edit
+    @pattern = Pattern.find(params[:id])
+    render :edit
   end
 
   def update
+    @pattern = Pattern.find(params[:id])
+    @pattern.update(pattern_params)
+    render :show
   end
 
   def destroy
+    @pattern = Pattern.find(params[:id])
+    @pattern.destroy
+    redirect_to patterns_url
   end
 
   def show
+    @pattern = Pattern.find(params[:id])
+    @current_user = current_user
+    render :show
   end
 
   private
 
   def pattern_params
-
+    params.require(:pattern).permit(:name, :category, :yarn_name, :yarn_weight, :stitch_col,
+      :stitch_row, :swatch, :swatch_stitch, :needles, :amount_yarn, :sizes, :price, :notes)
   end
 
 end
