a9ce95c85a086f3687a444af69fc184f9e417683
Candace Schenk <candace.schenk@gmail.com>
2014-05-01 09:36:01 -0400

    adds a seed file

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ end
 group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
+  gem 'faker'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |i|
+  u = User.create(email: Faker::Internet.safe_email, name: Faker::Internet.user_name, password: "123123", bio: Faker::Company.bs)
+end
+
+a = (1...100).to_a
+b = (1...10).to_a
+1000.times do |i|
+  # user connections
+  a.shuffle!
+  u = User.find(a[0])
+  u2 = User.find(a[1])
+  u.leaders << u2
+  
+  # messages
+  body = "Hi there #{u2.show_name}, its nice to see you around here, #{u.show_name}"
+  m = u.sent_messages.create(body: body, receiver_id: u2.id)
+  
+  # patterns
+  name = Faker::Commerce.product_name
+  yarn_name = Faker::Company.name
+  yarn_weight = b.shuffle.first.to_s
+  stitch_col = b.shuffle.first.to_s
+  stitch_row = b.shuffle.first.to_s
+  swatch = "4 inches"
+  swatch_stitch = "stockenette stitch"
+  needles = "US " + (2 * b.suffle.first).to_s
+  amount_yarn = "Enough"
+  price = "free"
+  u.designs.create(name: name, yarn_name: yarn_name, yarn_weight: yarn_weight, stitch_col: stitch_col
+                    stitch_row: stitch_row, swatch: swatch, swatch_stitch: swatch_stitch, needles: needles
+                    amount_yarn: amount_yarn, price: price)
+end
