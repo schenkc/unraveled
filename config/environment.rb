25aa7c0cc798c640805652e83dce45743d14babb
Candace Schenk <candace.schenk@gmail.com>
2014-05-03 12:13:11 -0400

    sets up sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,20 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Unraveled::Application.initialize!
+
+if Rails.env.production?
+  # only send real emails in production; use Sengrid
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method ||= :smtp
+elsif Rails.env.development?
+  # Remember the letter_opener gem? It won't send real emails; it
+  # just opens them in another tab.
+  ActionMailer::Base.delivery_method = :letter_opener
+end
