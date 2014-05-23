456de2660eff02f7589757ad2b7b263e39041383
Candace Schenk <candace.schenk@gmail.com>
2014-05-23 12:28:44 -0400

    still trying to get javascript to be served up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Unraveled::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( man_for_jquery.js carrousel_man.js application.js )
-
+  config.assets.digest = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
